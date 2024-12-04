1b1a91b9769cef5ff938100c61c50a1f8f912932
janejira <janjira.kikik@gmail.com>
2024-12-04 23:21:21 +0700

    add push api

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const app = express();
 
 app.use(express.json());
 
+let items = [];
+
 app.get('/api/items',(req ,res) =>{
-    res.send('Product all');
+     res.json({
+        message : "Product list",
+        data: items
+    });
 });
+
+app.post('/api/items',(req,res) =>{
+    const {name,price} = req.body; //รับข้อมูลจาก ฺbody ที่ส่งมา
+    if (name && price) {
+        // สร้างผลิตภัณฑ์ใหม่
+        const newItem = { name, price };
+        // เพิ่มผลิตภัณฑ์ใหม่ลงในอาร์เรย์ items
+        items.push(newItem);
+        // ส่งกลับข้อมูลที่ได้รับจากคำขอ POST
+        res.status(201).json({
+            message: 'Product created',
+            data: newItem
+        });
+    } else {
+        res.status(400).json({ message: 'Missing name or price' });
+    }
+});
+
 
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
